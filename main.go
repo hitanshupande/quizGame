e7548b16bd022c0c6a1ac0f8f13f0777ffb00500
Hitanshu Pande <hitanshu.pande@gmail.com>
2021-01-22 00:20:19 -0500

    quiz game implementation

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import ("flag"
+		"os"
+		"fmt"
+		"encoding/csv"
+		"strings")
+
+func main() {
+	//get filename from flag
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+	//read file
+	//1. Open file
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file: %s\n", *csvFilename))
+	}
+	//2. Read file
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	
+	problems := parseLines(lines)
+	
+
+	//var to keep score
+	correct := 0
+
+	//iterate and print questions, get answers
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+
+
+//wrap error handling in a method
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
